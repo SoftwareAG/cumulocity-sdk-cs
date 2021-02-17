20f52f2616ac6ce3e4e12076551a79688cb0bb0a
szczepaniak_p <piotr.szczepaniak@softwareag.com>
2021-02-17 14:51:29 +0100

    new container

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 deploy() {
 
 YUM_USR=hudson
-YUM_USR_KEY=~/.ssh/id_rsa
 YUM_SRV=yum.cumulocity.com
 YUM_DEST_DIR=/var/www/resources/cssdk/releases
 
-for var in "$@"
+for package in "$@"
 do
-    echo "deploy $var to $YUM_SRV:${YUM_DEST_DIR}"
-    scp -Cr -i ${YUM_USR_KEY} $var ${YUM_USR}@${YUM_SRV}:${YUM_DEST_DIR}
+    echo "deploy $package to $YUM_SRV:${YUM_DEST_DIR}"
+    scp -o StrictHostKeyChecking=no  -Cr $package ${YUM_USR}@${YUM_SRV}:${YUM_DEST_DIR}
+ #   dotnet nuget push $package  -k <token>  -s < source https://api.nuget.org/v3/index.json>  
 done
 
 }
